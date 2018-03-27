7b423ce692aab6e0296b4ddbc538c10c5551c2bc
Kevin Wahome <wahomekevin@gmail.com>
2018-03-27 23:53:22 +0300

    Switched to gulp

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+var gulp        = require('gulp');
+var browserSync = require('browser-sync').create();
+var sass        = require('gulp-sass');
+
+// Static Server + watching scss/html files
+gulp.task('serve', function() {
+
+  browserSync.init({
+      server: "./public",
+      middleware: [
+        {
+            route: "/css",
+            handle: function (req, res, next) {
+                next();
+            }
+        }
+    ]
+  });
+
+  gulp.watch("src/scss/**/*.scss", ['sass']);
+  gulp.watch("public/*.html").on('change', browserSync.reload);
+});
+
+// Compile sass into CSS & auto-inject into browsers
+gulp.task('sass', function() {
+  return gulp.src("src/scss/main.scss")
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest("public/css"))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('default', ['sass', 'serve']);
